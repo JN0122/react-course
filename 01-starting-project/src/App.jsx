c0cb24cbe78fd7687881f7411f669f122bafae04
Jakub Niewelt <jakubniewelt@icloud.com>
2024-08-06 21:26:37 +0200

    feat: final version of project

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,7 +1,39 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import UserInput from "./components/UserInput";
+import { ResultTable } from "./components/ResultTable";
+
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 15000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const isInputValid = userInput.duration >= 1;
+
+  function handleUserInputChange(variable, value) {
+    setUserInput((prev) => {
+      return { ...prev, [variable]: +value };
+    });
+  }
+
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <Header />
+      <main>
+        <UserInput
+          userInput={userInput}
+          onValueChange={handleUserInputChange}
+        />
+        {!isInputValid && (
+          <p className="center">Please insert a duration greater than 0!</p>
+        )}
+        {isInputValid && <ResultTable userInput={userInput} />}
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
