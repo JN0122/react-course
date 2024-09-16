8054eb97e84dff341664c6bccfcbc331d59cca2f
Jakub Niewelt <jakubniewelt@icloud.com>
2024-09-16 15:55:55 +1000

    feat: implementing useEffect and useCallback

--- 11 Side Effects useEffect/src/components/DeleteConfirmation.jsx
+++ 11 Side Effects useEffect/src/components/DeleteConfirmation.jsx
@@ -1,4 +1,16 @@
+import { useEffect } from "react";
+
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      onCancel();
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [onCancel]);
+
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
