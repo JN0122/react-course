ba277e43067062f7418605a433727334ce455a72
Jakub Niewelt <jakubniewelt@icloud.com>
2024-09-22 13:53:34 +1200

    feat: prepare main components

--- 12 Demo Project - React Quiz/src/App.jsx
+++ 12 Demo Project - React Quiz/src/App.jsx
@@ -1,3 +1,22 @@
-function App() {}
+import Header from "./components/Header";
+import Quiz from "./components/Quiz/Quiz";
+import Summary from "./components/Summary";
+
+import { useState } from "react";
+
+function App() {
+  const [isQuizRunning, setIsQuizRunning] = useState(true);
+
+  let content = <Summary />;
+  if (isQuizRunning) {
+    content = <Quiz />;
+  }
+  return (
+    <>
+      <Header />
+      {content}
+    </>
+  );
+}
 
 export default App;
