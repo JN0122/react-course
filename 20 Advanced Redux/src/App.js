c0a8d67f040dec377999afb659f010cef66478b3
Jakub Niewelt <jakubniewelt@icloud.com>
2025-08-15 12:57:11 +0200

    feat: handle async Redux with useEffect

--- 20 Advanced Redux/src/App.js
+++ 20 Advanced Redux/src/App.js
@@ -2,9 +2,19 @@ import { useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { useEffect } from "react";
 
 function App() {
   const showCart = useSelector((state) => state.ui.showCart);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch(
+      "https://advanced-redux-4262f-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+      { method: "PUT", body: JSON.stringify(cart) }
+    );
+  }, [cart]);
+
   return (
     <Layout>
       {showCart && <Cart />}
@@ -14,3 +24,4 @@ function App() {
 }
 
 export default App;
+
