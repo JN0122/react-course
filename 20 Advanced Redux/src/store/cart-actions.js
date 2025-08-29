a9b42e687bf8de9551b6f23c9e68ad18185b135f
Jakub Niewelt <jakubniewelt@icloud.com>
2025-08-30 00:42:18 +0200

    refactor: move action to separate file

---
+++ 20 Advanced Redux/src/store/cart-actions.js
@@ -0,0 +1,43 @@
+import { showNotification } from "./ui";
+
+export const sendCardData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      showNotification({
+        status: "pending",
+        title: "Sending...",
+        message: "Sending cart data!",
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://advanced-redux-4262f-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+        { method: "PUT", body: JSON.stringify(cart) }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed.");
+      }
+    };
+
+    try {
+      await sendRequest();
+      dispatch(
+        showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!",
+        })
+      );
+    } catch (error) {
+      dispatch(
+        showNotification({
+          status: "error",
+          title: "Error!",
+          message: error.message,
+        })
+      );
+    }
+  };
+};
