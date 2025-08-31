9ea68cc0347ae00922ad577b2b2514982a46996f
Jakub Niewelt <jakubniewelt@icloud.com>
2025-08-31 16:52:54 +0200

    feat: handle different types of errors with useRouterError hook

--- 21 Routing/src/pages/Error.js
+++ 21 Routing/src/pages/Error.js
@@ -1,7 +1,30 @@
 import React from "react";
 
+import { useRouteError } from "react-router-dom";
+
+import PageContent from "../components/PageContent";
+import MainNavigation from "../components/MainNavigation";
+
 const ErrorPage = () => {
-  return <div>Something went wrong!</div>;
+  const response = useRouteError();
+
+  let title = "An error occurred!";
+  let message = "Something went wrong!";
+
+  if (response?.status === 404) {
+    title = "Cannot find element!";
+  } else if (response?.status === 500) {
+    message = JSON.parse(response.data).message;
+  }
+
+  return (
+    <>
+      <MainNavigation />
+      <PageContent title={title}>
+        <p>{message}</p>
+      </PageContent>
+    </>
+  );
 };
 
 export default ErrorPage;
