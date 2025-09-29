558ea1167861a4c57c30ae68862d78fe92e1aea1
Jakub Niewelt <jakubniewelt@icloud.com>
2025-09-29 22:31:23 +0200

    feat: create signup/login action

--- 22 Authentication/frontend/src/components/MainNavigation.js
+++ 22 Authentication/frontend/src/components/MainNavigation.js
@@ -1,7 +1,7 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
-import classes from './MainNavigation.module.css';
-import NewsletterSignup from './NewsletterSignup';
+import classes from "./MainNavigation.module.css";
+import NewsletterSignup from "./NewsletterSignup";
 
 function MainNavigation() {
   return (
@@ -39,6 +39,16 @@ function MainNavigation() {
               Newsletter
             </NavLink>
           </li>
+          <li>
+            <NavLink
+              to="/auth?mode=login"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Authentication
+            </NavLink>
+          </li>
         </ul>
       </nav>
       <NewsletterSignup />
@@ -47,3 +57,4 @@ function MainNavigation() {
 }
 
 export default MainNavigation;
+
