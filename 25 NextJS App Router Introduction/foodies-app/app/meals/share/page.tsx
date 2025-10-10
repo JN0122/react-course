5e83d65a301ddcc46bc4e8a73914116284cb4ff2
Jakub Niewelt <jakubniewelt@icloud.com>
2025-10-10 22:53:24 +0200

    feat: loading button when submiting meal

--- 25 NextJS App Router Introduction/foodies-app/app/meals/share/page.tsx
+++ 25 NextJS App Router Introduction/foodies-app/app/meals/share/page.tsx
@@ -3,6 +3,7 @@ import { NextPage } from "next";
 import classes from "./page.module.css";
 import ImagePicker from "@/components/image-picker/image-picker";
 import { shareMeal } from "@/lib/actions/meals";
+import MealsSubmitButton from "@/components/meals/meals-submit-button";
 
 const ShareMealsPage: NextPage = ({}) => {
   return (
@@ -44,7 +45,7 @@ const ShareMealsPage: NextPage = ({}) => {
           </p>
           <ImagePicker label="Meal Image" name="image" />
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsSubmitButton />
           </p>
         </form>
       </main>
