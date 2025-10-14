b05e73ff2f98ca5b0da4d43a585635454689a585
Jakub Niewelt <jakubniewelt@icloud.com>
2025-10-14 18:45:27 +0200

    feat: SSG the meetups

--- 25 NextJS App Router Introduction/meetup-app/pages/[meetupId]/index.tsx
+++ 25 NextJS App Router Introduction/meetup-app/pages/[meetupId]/index.tsx
@@ -1,14 +1,44 @@
+import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from "next";
+
 import MeetupDetail from "@/components/meetups/MeetupDetail";
 import { Meetup } from "@/lib/types/meetup";
 
-const DUMMY_MEETUP: Meetup = {
-  id: "m1",
-  title: "First Meetup",
-  image: "http://media.krakow.travel/photos/18784/noresize.jpg",
-  address: "123 Main St, City",
-  description: "This is the first meetup.",
+type Props = {
+  meetupData: Meetup;
 };
 
-export default function MeetupDetails() {
-  return <MeetupDetail meetupData={DUMMY_MEETUP} />;
+type Params = {
+  meetupId: string;
+};
+
+export const getStaticPaths: GetStaticPaths = () => {
+  return {
+    paths: [{ params: { meetupId: "m1" } }],
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps<Props, Params> = async ({
+  params,
+}) => {
+  if (!params?.meetupId) {
+    return { notFound: true };
+  }
+
+  const DUMMY_MEETUP: Meetup = {
+    id: params.meetupId,
+    title: "First Meetup",
+    image: "http://media.krakow.travel/photos/18784/noresize.jpg",
+    address: "123 Main St, City",
+    description: "This is the first meetup.",
+  };
+
+  return { props: { meetupData: DUMMY_MEETUP } };
+};
+
+export default function MeetupDetails({
+  meetupData,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  return <MeetupDetail meetupData={meetupData} />;
 }
+
