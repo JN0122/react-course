0969af746944d1b9263068e6513476ef37181807
Jakub Niewelt <jakubniewelt@icloud.com>
2025-10-14 15:41:39 +0200

    feat: create homepage with meetups

---
+++ 25 NextJS App Router Introduction/meetup-app/pages/index.tsx
@@ -0,0 +1,33 @@
+import MeetupList from "@/components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "First Meetup",
+    image: "http://media.krakow.travel/photos/18784/noresize.jpg",
+    address: "123 Main St, City",
+    description: "This is the first meetup.",
+  },
+  {
+    id: "m2",
+    title: "Second Meetup",
+    image: "http://media.krakow.travel/photos/18784/noresize.jpg",
+    address: "456 Elm St, City",
+    description: "This is the second meetup.",
+  },
+  {
+    id: "m3",
+    title: "Third Meetup",
+    image: "http://media.krakow.travel/photos/18784/noresize.jpg",
+    address: "789 Oak St, City",
+    description: "This is the third meetup.",
+  },
+];
+
+export default function HomePage() {
+  return (
+    <>
+      <MeetupList meetups={DUMMY_MEETUPS} />
+    </>
+  );
+}
