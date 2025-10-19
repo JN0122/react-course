0b5a2ed0daefc4ed077497a330e68a0fcecaf18d
Jakub Niewelt <jakubniewelt@icloud.com>
2025-10-19 22:34:36 +0200

    test: check Greeting component state

--- 28 Testing/src/components/Greeting.jsx
+++ 28 Testing/src/components/Greeting.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Greeting() {
+  const [isButtonClicked, setIsButtonClicked] = useState(false);
+
+  const handleButtonClick = () => {
+    setIsButtonClicked(true);
+  };
+
   return (
     <div>
       <h2>Hello World!</h2>
-      <p>It is good to see you!</p>
+      <p>{isButtonClicked ? "It is good to see you!" : "Click button!"}</p>
+      <button onClick={handleButtonClick}>Click me!</button>
     </div>
   );
 }
--- 28 Testing/src/components/Greeting.test.js
+++ 28 Testing/src/components/Greeting.test.js
@@ -1,16 +1,34 @@
 import { render, screen } from "@testing-library/react";
 import Greeting from "./Greeting";
+import userEvent from "@testing-library/user-event";
 
 describe("Greeting component", () => {
   test("renders Hello World as a text", () => {
-    // Arrange
     render(<Greeting />);
-
-    // Act
-    // ... nothing
-
-    // Assert
     const helloWorldElement = screen.getByText(/Hello world/i);
     expect(helloWorldElement).toBeInTheDocument();
+  });
+  test("renders click button when button is not clicked", () => {
+    render(<Greeting />);
+    const element = screen.getByText("click button", { exact: false });
+    expect(element).toBeInTheDocument();
+  });
+  test("renders good to see you when button is clicked", () => {
+    render(<Greeting />);
+
+    const button = screen.getByRole("button");
+    userEvent.click(button);
+
+    const element = screen.getByText("good to see", { exact: false });
+    expect(element).toBeInTheDocument();
+  });
+  test("not renders click button when button is clicked", () => {
+    render(<Greeting />);
+
+    const button = screen.getByRole("button");
+    userEvent.click(button);
+
+    const element = screen.queryByText("click button", { exact: false });
+    expect(element).toBeNull();
   });
 });
