cda1aac6f4c3b4f26efd9af93add3f86e5dd6206
Jakub Niewelt <jakubniewelt@icloud.com>
2025-10-21 16:35:31 +0200

    feat: await server promise with use hook

--- 25b RSC, Suspense, Server Actions/app/page.js
+++ 25b RSC, Suspense, Server Actions/app/page.js
@@ -1,9 +1,22 @@
-import DataFetchingDemo from "@/components/DataFetchingDemo";
+import { Suspense } from "react";
+import fs from "node:fs/promises";
 
-export default function Home() {
+import UserPromiseDemo from "@/components/UsePromisesDemo";
+
+export default async function Home() {
+  const usersPromise = new Promise((resolve) =>
+    setTimeout(async () => {
+      const data = await fs.readFile("dummy-db.json", "utf-8");
+      const users = JSON.parse(data);
+      resolve(users);
+    }, 2000)
+  );
+
   return (
     <main>
-      <DataFetchingDemo />
+      <Suspense fallback={"Loading..."}>
+        <UserPromiseDemo usersPromise={usersPromise} />
+      </Suspense>
     </main>
   );
 }
