ab8bd8f89babcc70f886d67b3de1f2556a0544f5
Jakub Niewelt <jakubniewelt@icloud.com>
2025-10-21 16:41:19 +0200

    feat: add error boundary to handle promise reject

--- 25b RSC, Suspense, Server Actions/app/page.js
+++ 25b RSC, Suspense, Server Actions/app/page.js
@@ -2,21 +2,25 @@ import { Suspense } from "react";
 import fs from "node:fs/promises";
 
 import UserPromiseDemo from "@/components/UsePromisesDemo";
+import ErrorBoundary from "@/components/ErrorBoundary";
 
 export default async function Home() {
-  const usersPromise = new Promise((resolve) =>
+  const usersPromise = new Promise((resolve, reject) =>
     setTimeout(async () => {
       const data = await fs.readFile("dummy-db.json", "utf-8");
       const users = JSON.parse(data);
-      resolve(users);
+      // resolve(users);
+      reject(new Error("Cannot load users"));
     }, 2000)
   );
 
   return (
     <main>
-      <Suspense fallback={"Loading..."}>
-        <UserPromiseDemo usersPromise={usersPromise} />
-      </Suspense>
+      <ErrorBoundary fallback={"Something went wrong!"}>
+        <Suspense fallback={"Loading..."}>
+          <UserPromiseDemo usersPromise={usersPromise} />
+        </Suspense>
+      </ErrorBoundary>
     </main>
   );
 }
