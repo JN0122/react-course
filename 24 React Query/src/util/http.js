1ef09de60c1e1ec3a53610433016fa9e33270cfb
Jakub Niewelt <jakubniewelt@icloud.com>
2025-11-02 20:43:19 +0100

    feat: handle query on submit

--- 24 React Query/src/util/http.js
+++ 24 React Query/src/util/http.js
@@ -1,5 +1,7 @@
-export async function fetchEvents() {
-  const response = await fetch("http://localhost:3000/events");
+export async function fetchEvents(searchQuery) {
+  const response = await fetch(
+    `http://localhost:3000/events${searchQuery ? `?search=${searchQuery}` : ""}`
+  );
 
   if (!response.ok) {
     const error = new Error("An error occurred while fetching the events");
