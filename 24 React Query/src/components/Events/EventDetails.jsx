27cd56086220f2612b83cc0fe3167ea7e31a481d
Jakub Niewelt <jakubniewelt@icloud.com>
2025-11-02 22:42:19 +0100

    feat: event details, add and remove event

--- 24 React Query/src/components/Events/EventDetails.jsx
+++ 24 React Query/src/components/Events/EventDetails.jsx
@@ -1,8 +1,76 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useNavigate, useParams } from "react-router-dom";
 
-import Header from '../Header.jsx';
+import Header from "../Header.jsx";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { deleteEvent, fetchEvent } from "../../util/http.js";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function EventDetails() {
+  const { id } = useParams();
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
+  const { mutate, isPending: isRemoving } = useMutation({
+    mutationFn: deleteEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["events"] });
+      navigate("/events");
+    },
+  });
+
+  const removeEventHandler = () => {
+    const removeEvent = confirm("Are you sure?");
+    if (removeEvent) mutate({ id });
+  };
+
+  const { data, isError, error } = useQuery({
+    queryKey: ["event-details", id],
+    queryFn: (context) => fetchEvent({ id, ...context }),
+  });
+
+  let content = (
+    <div id="event-details-content" className="center">
+      <p className="center">Loading event details...</p>
+    </div>
+  );
+
+  if (data)
+    content = (
+      <>
+        <header>
+          <h1>{data.title}</h1>
+          <nav>
+            <button onClick={removeEventHandler} disabled={isRemoving}>
+              {isRemoving ? "Removing..." : "Delete"}
+            </button>
+            <Link to="edit">Edit</Link>
+          </nav>
+        </header>
+        <div id="event-details-content">
+          <img src={`http://localhost:3000/${data.image}`} alt={data.title} />
+          <div id="event-details-info">
+            <div>
+              <p id="event-details-location">{data.location}</p>
+              <time
+                dateTime={`${data.date}T${data.time}`}
+              >{`${data.date} @ ${data.time}`}</time>
+            </div>
+            <p id="event-details-description">{data.description}</p>
+          </div>
+        </div>
+      </>
+    );
+
+  if (isError)
+    content = (
+      <div id="event-details-content">
+        <ErrorBlock
+          title="Error while fetching event details"
+          message={error.info?.message}
+        />
+      </div>
+    );
+
   return (
     <>
       <Outlet />
@@ -11,25 +79,8 @@ export default function EventDetails() {
           View all Events
         </Link>
       </Header>
-      <article id="event-details">
-        <header>
-          <h1>EVENT TITLE</h1>
-          <nav>
-            <button>Delete</button>
-            <Link to="edit">Edit</Link>
-          </nav>
-        </header>
-        <div id="event-details-content">
-          <img src="" alt="" />
-          <div id="event-details-info">
-            <div>
-              <p id="event-details-location">EVENT LOCATION</p>
-              <time dateTime={`Todo-DateT$Todo-Time`}>DATE @ TIME</time>
-            </div>
-            <p id="event-details-description">EVENT DESCRIPTION</p>
-          </div>
-        </div>
-      </article>
+      <article id="event-details">{content}</article>
     </>
   );
 }
+
